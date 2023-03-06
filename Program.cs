e6bfdcd2a703a0c433228ec98addb51ca096ca8e
kundan <goswamikundan123@gmail.com>
2023-03-06 15:52:10 +0530

    Adding file which have analyze mood function.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mood Analyser Problem's solution Master Branch.");
+            //Console.WriteLine("Welcome to Mood Analyser Problem's solution Master Branch.");
+
+            string message = "I am happy";
+
+            AnalyzeMood(message);
+        }
+
+        public static void AnalyzeMood(string message)
+        {
+            string happy = "I am happy";
+            string sad = "I am sad";
+
+            if (message == happy)
+            {
+                Console.WriteLine("Mood: Happy");
+            }
+            else if (message == sad)
+            {
+                Console.WriteLine("Mood: Sad");
+            }
         }
     }
 }
