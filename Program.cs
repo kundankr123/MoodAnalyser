58573b9d7b9b324f8244ce7150b0cd26941ec1f6
kundan <goswamikundan123@gmail.com>
2023-03-06 15:20:21 +0530

    Adding file which displays Greeting message.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem's solution Master Branch.");
+        }
+    }
+}
