d1b95de2c02ec640f6da6470fd2616bbb3d10952
kundan <goswamikundan123@gmail.com>
2023-03-07 16:01:21 +0530

    Adding file having test cases for reflection problem.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,35 +5,38 @@ namespace MstestMoodanalyser
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        [ExpectedException(typeof(Moodanalysisexc))]
-        public void Analyze_NullMessage_ThrowsMoodAnalysisException()
-        {
-            //Arrange
-            string message = null;
-
-            //Act
-            Analyzemood moodAnalyzer = new Analyzemood(message);
-
-
-        }
 
         [TestMethod]
-        [ExpectedException(typeof(Moodanalysisexc), "Exception Raised: No message was passed.")]
-        public void Analyze_NullMessage_ThrowsMoodAnalysisExceptionWithMessage()
+        public void GivenHappyMessage_UsingReflection_ShouldReturnHappyMood()
         {
             // Arrange
-            string message = null;
+            string message = "I am happy";
+            string expectedMood = "happy";
 
-            // Act and Assert
+            // Act
+            string mood = MoodanalyzerFactory.InvokeAnalyzeMoodMethod(message);
+
+            // Assert
+            Assert.AreEqual(expectedMood, mood);
+        }
+
+
+        [TestMethod]
+        public void GivenHappyMessage_WhenImproperMethod_ShouldThrowMoodAnalysisException()
+        {
+            // Arrange
+            string message = "I am happy";
+            string methodName = "WrongMethodName";
+
             try
             {
-                Analyzemood moodAnalyzer = new Analyzemood(message);
+                // Act
+                string mood = MoodanalyzerFactory.InvokeAnalyzeMoodMethod(message);
             }
             catch (Moodanalysisexc ex)
             {
-                Assert.AreEqual("Exception Raised: No message was passed.", ex.Message);
-                throw;
+                // Assert
+                Assert.AreEqual("No such method found", ex.Message);
             }
         }
     }
